4e3fab7ba7734e24b884ff068d98060cc6167c0c
Jason <bilafish@gmail.com>
2021-02-01 15:52:35 +0800

    tweak: add update default payment method &  create subscription logic

--- src/functions/create-subscription.js
+++ src/functions/create-subscription.js
@@ -1,22 +1,42 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  const { paymentMethodId, customerId } = JSON.parse(event.body);
+  const { paymentMethodId, customerId, priceId } = JSON.parse(event.body);
   let paymentMethod;
   try {
     paymentMethod = await stripe.paymentMethods.attach(paymentMethodId, {
       customer: customerId,
     });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ paymentMethod }),
-    };
   } catch (error) {
     console.log(error);
     return {
       statusCode: 200,
-      body: JSON.stringify({ error }),
+      body: JSON.stringify({
+        error: {
+          message: error.message,
+        },
+      }),
     };
   }
+
+  let updateCustomerDefaultPaymentMethod = await stripe.customers.update(
+    customerId,
+    {
+      invoice_settings: {
+        default_payment_method: paymentMethod.id,
+      },
+    }
+  );
+
+  // Create subscription
+  const subscription = await stripe.subscriptions.create({
+    customer: customerId,
+    items: [{ price: priceId }],
+    expand: ["latest_invoice.payment_intent"],
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(subscription),
+  };
 };
